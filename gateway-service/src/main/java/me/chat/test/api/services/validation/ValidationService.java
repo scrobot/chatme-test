76c6c8cfab1d660a3e89373b51247da32d342927
Alex Scrobot <scrobot91@gmail.com>
2020-02-28 18:59:08 +0700

    added documentation to the contracts

--- gateway-service/src/main/java/me/chat/test/api/services/validation/ValidationService.java
+++ gateway-service/src/main/java/me/chat/test/api/services/validation/ValidationService.java
@@ -1,5 +1,13 @@
 package me.chat.test.api.services.validation;
 
+/**
+ * A contract delegating requests to validation remote service, like a proxy.
+ */
 public interface ValidationService {
+
+    /**
+     * Terminal operation that starting listen to the validation response
+     */
     void startValidationProcess();
+
 }
